a99d3992926e9606dd0a5f8527cf2f016d626f88
Ayeshaatique <ayesha.atiquee@gmail.com>
2019-09-11 01:07:12 -0700

    Test Branching

---
+++ app/src/main/java/com/example/roubaisha/counter/FuzulActivity.java
@@ -0,0 +1,13 @@
+package com.example.roubaisha.counter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FuzulActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fuzul);
+    }
+}
