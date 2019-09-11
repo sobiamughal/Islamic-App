6f4a403b7102bdea41a0f9841e7ca06825fb4ab7
Ayeshaatique <ayesha.atiquee@gmail.com>
2019-09-11 01:08:55 -0700

    Test Branching...Again

--- app/src/main/java/com/example/roubaisha/counter/FuzulActivity.java
+++ app/src/main/java/com/example/roubaisha/counter/FuzulActivity.java
@@ -2,6 +2,7 @@ package com.example.roubaisha.counter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class FuzulActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class FuzulActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fuzul);
+
+
+        Log.d("222","eee");
     }
 }
